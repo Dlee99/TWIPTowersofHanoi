895eff9ae49e8c27706ec5a2a5b92518bf90fbec
Dlee99 <Dereklee9ab@aol.com>
2017-01-21 17:43:53 -0500

    ididit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many discs would you like to solve for?");
         int discs = sc.nextInt();
-        nextMove.Move(discs, "1", "2", "3");
+        nextMove.Move(discs, "A", "B", "C");
     }
 }
--- src/com/company/nextMove.java
+++ src/com/company/nextMove.java
@@ -1,9 +1,14 @@
 package com.company;
-import java.util.Scanner;
+
 public class nextMove {
-    public static void Move(int n, String A, String B, String C) {
+    public static void Move(int n, String T1, String T2, String T3) {
         if(n == 1){
-            Move(n - 1, A, C, B);
+            System.out.println("Move from " + T1 + " to " + T3);
+        }
+        else{
+            Move(n - 1, T1, T3, T2);
+            System.out.println("Move from " + T1 + " to " + T3);
+            Move(n - 1, T2, T1, T3);
         }
     }
 }
